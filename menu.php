6412220c5ee6121fe0f1373ebfb9cf0b158a7608
patulo <patulo@zend-patulo.fjeclot.net>
2023-06-09 17:41:49 +0200

    commit 1

---
+++ menu.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>
+			PÀGINA WEB DEL MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP
+		</title>
+	</head>
+	<body>
+		<h2> MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h2>
+		<h3> <b>En construcció!!!!!!!!!!!</b> </h3>
+		<a href="http://zend-patulo.fjeclot.net/autent/viewUser.php">Ver usuario</a>
+		<a href="http://zend-patulo.fjeclot.net/autent/index.php">Torna a la pàgina inicial</a>
+	</body>
+</html>
