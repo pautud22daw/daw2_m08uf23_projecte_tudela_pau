1f22360c8181fb1835c34b612c058b03de39f3cb
pautud22daw <113779199+pautud22daw@users.noreply.github.com>
2023-06-10 10:07:55 +0200

    Update modUser.php

--- modUser.php
+++ modUser.php
@@ -44,39 +44,39 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 	</head>
 	<body>
 		<form action="http://zend-patulo.fjeclot.net/projecte/modUser.php" method="POST">
-            <label for="uid">UID:</label>
-            <input type="text" name="uid" id="uid" required><br>
-            <label for="unorg">Unidad Organizativa:</label>
-            <input type="text" name="unorg" id="unorg" required><br>
-            <label>Atributo a modificar:</label><br>
-            <input type="radio" id="uidNumber" name="atribut" value="uidNumber" required>
-            <label for="uidNumber">uidNumber</label><br>
-            <input type="radio" id="gidNumber" name="atribut" value="gidNumber">
-            <label for="gidNumber">gidNumber</label><br>
-            <input type="radio" id="directorio" name="atribut" value="homeDirectory">
-            <label for="directorio">Directorio personal</label><br>
-            <input type="radio" id="shell" name="atribut" value="loginShell">
-            <label for="shell">Shell</label><br>
-            <input type="radio" id="cn" name="atribut" value="cn">
-            <label for="cn">cn</label><br>
-            <input type="radio" id="sn" name="atribut" value="sn">
-            <label for="sn">sn</label><br>
-            <input type="radio" id="givenName" name="atribut" value="givenName">
-            <label for="givenName">givenName</label><br>
-            <input type="radio" id="postalAddress" name="atribut" value="postalAddress">
-            <label for="postalAddress">PostalAddress</label><br>
-            <input type="radio" id="mobile" name="atribut" value="mobile">
-            <label for="mobile">mobile</label><br>
-            <input type="radio" id="telephoneNumber" name="atribut" value="telephoneNumber">
-            <label for="telephoneNumber">telephoneNumber</label><br>
-            <input type="radio" id="title" name="atribut" value="title">
-            <label for="title">title</label><br>
-            <input type="radio" id="description" name="atribut" value="description">
-            <label for="description">description</label><br>
-            <label for="nou_contingut">Nuevo valor:</label>
-            <input type="text" name="nou_contingut" id="nou_contingut" required>
-            <input type="submit" value="Modificar Usuario">
-        </form>
-        <a href="http://zend-patulo.fjeclot.net/projecte/menu.php">Torna a la pàgina inicial</a>
+			<label for="uid">UID:</label>
+			<input type="text" name="uid" id="uid" required><br>
+			<label for="unorg">Unidad Organizativa:</label>
+			<input type="text" name="unorg" id="unorg" required><br>
+			<label>Atributo a modificar:</label><br>
+			<input type="radio" id="uidNumber" name="atribut" value="uidNumber" required>
+			<label for="uidNumber">uidNumber</label><br>
+			<input type="radio" id="gidNumber" name="atribut" value="gidNumber">
+			<label for="gidNumber">gidNumber</label><br>
+			<input type="radio" id="directorio" name="atribut" value="homeDirectory">
+			<label for="directorio">Directorio personal</label><br>
+			<input type="radio" id="shell" name="atribut" value="loginShell">
+			<label for="shell">Shell</label><br>
+			<input type="radio" id="cn" name="atribut" value="cn">
+			<label for="cn">cn</label><br>
+			<input type="radio" id="sn" name="atribut" value="sn">
+			<label for="sn">sn</label><br>
+			<input type="radio" id="givenName" name="atribut" value="givenName">
+			<label for="givenName">givenName</label><br>
+			<input type="radio" id="postalAddress" name="atribut" value="postalAddress">
+			<label for="postalAddress">PostalAddress</label><br>
+			<input type="radio" id="mobile" name="atribut" value="mobile">
+			<label for="mobile">mobile</label><br>
+			<input type="radio" id="telephoneNumber" name="atribut" value="telephoneNumber">
+			<label for="telephoneNumber">telephoneNumber</label><br>
+			<input type="radio" id="title" name="atribut" value="title">
+			<label for="title">title</label><br>
+			<input type="radio" id="description" name="atribut" value="description">
+			<label for="description">description</label><br>
+			<label for="nou_contingut">Nuevo valor:</label>
+			<input type="text" name="nou_contingut" id="nou_contingut" required>
+			<input type="submit" value="Modificar Usuario">
+		</form>
+		<a href="http://zend-patulo.fjeclot.net/projecte/menu.php">Torna a la pàgina inicial</a>
 	</body>
 </html>
